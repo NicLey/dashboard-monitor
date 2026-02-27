1fa66b9ee41d8ac3e0a3cd58e9d50418a9ede029
Nicolás Andrés Godoy Canales <143671855+NicLey@users.noreply.github.com>
2026-02-27 19:49:53 -0300

    Delete test_env.py

--- test_env.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-print("DASHBOARD_USER:", os.getenv("DASHBOARD_USER"))
-print("TWILIO_SID:", os.getenv("TWILIO_SID"))
-print("FALLBACK_PHONE:", os.getenv("FALLBACK_PHONE"))
